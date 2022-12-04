fdb095c94ef3565115e9aff2d4ad2f3317af41b0
JamilTalukder103 <mdjamiltalukder143@gmail.com>
2022-12-04 20:43:58 +0600

    done

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ const carObject = {
 const BikeObject = {
       vechile: 'Bike',
       imageUrl: "https://images.unsplash.com/photo-1558981403-c5f9899a28bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YmlrZXxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60",
-      farePerKilo: 3,
+      farePerKilo: 2,
       capacity: 14,
       discription: 'This is a wider card with supporting card with supporting tex'
 
@@ -18,11 +18,12 @@ const BikeObject = {
 const BusObject = {
       vechile: 'Bus',
       imageUrl: "https://plus.unsplash.com/premium_photo-1661916408325-5280388bf1e3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YnVzfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-      farePerKilo: 3,
+      farePerKilo: 4,
       capacity: 2,
       discription: 'This is a wider card with supporting card with supporting tex'
 
 };
+const serviceArray = [BikeObject, BusObject, carObject];
 function displayService(service) {
       const stringified = JSON.stringify(service)
       const mainSection = document.getElementById('main-section');
@@ -48,19 +49,59 @@ function displayService(service) {
       `;
       mainSection.appendChild(div)
 }
-displayService(BikeObject)
-displayService(BusObject)
-displayService(carObject);
+
+function displayAllArticle(arry) {
+      for (let i = 0; i < arry.length; i++) {
+            const element = arry[i];
+            displayService(element)
+      }
+}
+document.getElementById("search-btn").addEventListener("click", function () {
+      const inputValue = document.getElementById('search-value').value;
+      for (let i = 0; i < serviceArray; i++) {
+            const element = serviceArray[i];
+            if (inputValue == element.vechile) {
+                  console.log(element)
+            }
+      }
+})
+displayAllArticle(serviceArray);
 function handelBooking(service) {
+      const stringified = JSON.stringify(service)
       const modalBody = document.getElementById('modal-body');
       modalBody.innerHTML = `
          <div class="card mx-auto" style="width: 400px;">
   <img src="${service.imageUrl}" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">vechile mood : ${service.vechile}</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> <p class="card-text"><small class="text-muted">Fare per kilo : ${service.farePerKilo}</small> <small>capacity : ${service.capacity}</small> </p>
+
+     <p >Fare : <small id="fare">0</small> </p>
+     <p >Tax : <small id="tax">0</small> </p>
+     <p >Total Cost : <small id="total-cost">0</small> </p>
   </div>
+  <div class="d-flex flex-column">
+      <input class="form-control mt-2 me-2" id="distance-input" type="number" placeholder="how many    kilometer?" aria-label="Search">
+      <input class="form-control mt-2 me-2" id="quntity-input" type="number" placeholder="how many people" aria-label="Search">
+      <button onclick='calculateCost(${stringified})' class="btn btn-outline-success mt-2" type="submit">Submit</button>
+</div>
 </div>
       `;
 }
+
+function calculateCost(service) {
+      const quntityInput = document.getElementById('quntity-input').value;
+      const distanceInput = document.getElementById('distance-input').value;
+
+      const fareDiv = document.getElementById('fare');
+      const totalFareCost = quntityInput * distanceInput * service.farePerKilo;
+
+
+      fareDiv.innerHTML = totalFareCost;
+      console.log(fareDiv)
+
+}
+
+
+
 
